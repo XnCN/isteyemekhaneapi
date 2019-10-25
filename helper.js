80b84607179f60b0e344cd88f515ffe85ec0af61
Eray Han <erayhan@Eray-MacBook-Pro.local>
2019-10-26 00:47:51 +0300

    regex not found error fix

--- helper.js
+++ helper.js
@@ -2,11 +2,16 @@ const prepareFoodInformations = (foods)=>{
     var regex = /(.*?) \( (\d+) Cal - (\d+) Gr \)/;
     return foods.map(food=>{
         const foodWithInfo = food.match(regex);
-        return {
-            Name:foodWithInfo[1].toLocaleUpperCase('tr').trim(),
-            Cal:foodWithInfo[2],
-            Gr:foodWithInfo[3]
+        try{
+            return {
+                Name:foodWithInfo[1].toLocaleUpperCase('tr').trim(),
+                Cal:foodWithInfo[2],
+                Gr:foodWithInfo[3]
+            }
+        }catch{
+            return {  }
         }
+       
     });
 }
 
